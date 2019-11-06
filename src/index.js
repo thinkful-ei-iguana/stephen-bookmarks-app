21c34d671e254955f2f06f965a01629e21902306
stephenschab <sschab689@gmail.com>
2019-11-06 14:14:02 -0700

    finished adding functions to main in index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ import 'normalize.css';
 import './index.css';
 
 import bookmark from './bookmark';
+import api from './api';
+import store from './store';
 
 const main = function() {
+  api.getBookmarks()
+    .then((bookmarks) => {
+      bookmarks.forEach(bookmark => store.addBookmark(bookmark));
+      bookmark.render();
+    });
+  bookmark.bindEventHandlers();
   bookmark.render();
 };
 
